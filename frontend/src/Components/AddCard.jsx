00ab4d891de39b041e4c69f638b9cab3118947fa
18120528 <57126456+18120528@users.noreply.github.com>
2024-09-17 19:22:26 +0700

    addList feat & toggle form

--- frontend/src/Components/AddCard.jsx
+++ frontend/src/Components/AddCard.jsx
@@ -1,24 +1,30 @@
-import { useRef } from "react"
+import { useRef,useState } from "react"
 //
 const AddCard = ({listId, socket}) => {
+  const [showForm,setShowForm]=useState(false)
   const taskRef=useRef(null)
 
   const handleSubmit=(e)=>{
     e.preventDefault()
     socket.emit("addCard",{listId, title: taskRef.current.value})
     taskRef.current.value=""
+    setShowForm(false)
   }
 
   return (
     <div className="add_card">
+      {showForm ? (
       <form onSubmit={handleSubmit} style={{backgroundColor:"lightsalmon",border: "3px solid black"}}>
-        <label htmlFor="taskContent">Add a card</label>
         <input type="text" required
         name="taskContent" id="taskContent"
         placeholder="Enter a name for this card..."
-        ref={taskRef}/>
+        ref={taskRef}/><br/>
         <button type="submit">Add Card</button>
+        <button onClick={()=>setShowForm(false)}>Close</button>
       </form>
+      ) : (
+        <label htmlFor="taskContent" onClick={()=>setShowForm(true)}>+ Add a card</label>
+      )}
     </div>
   )
 }
