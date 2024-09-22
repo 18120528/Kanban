35070963547e91fc8e1e767d0fa77da09cd8ea66
18120528 <57126456+18120528@users.noreply.github.com>
2024-09-22 23:47:20 +0700

    debug

--- backend/src/controllers/connectMongoDB.js
+++ backend/src/controllers/connectMongoDB.js
@@ -5,10 +5,10 @@ require("dotenv").config()
 const connectMongoDB=async ()=>{
     try {
       await mongoose.connect(process.env.MONGO_URI)
-      console.log(process.env.MONGO_URI)
       console.log("Connect to MongoDB successful!")
     } catch (error) {
-      console.log("Connect to MongoDB failed!")
+      console.log(process.env.MONGO_URI)
+      console.log(error)
     }
   }
   //
